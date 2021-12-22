9e2b0ff503b6cf92bbe8a818acf2f339c751632b
Irene <jaireni@hotmail.com>
2021-12-22 14:45:48 +0000

    gen comp lista-usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaUsuariosComponent } from './lista-usuarios/lista-usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaUsuariosComponent
   ],
   imports: [
     BrowserModule,
