2fa348f60728a6a390b283acf53ff4b5b6504f8f
DESKTOP-NM7UD0H <isabel0729.it@gmail.com>
2021-12-22 16:16:37 +0100

    creacion de array de series

--- src/app/components/lista-series/lista-series.component.ts
+++ src/app/components/lista-series/lista-series.component.ts
@@ -8,9 +8,30 @@ import { Serie } from 'src/app/interface/serie.interface';
 })
 export class ListaSeriesComponent implements OnInit {
 
+  arrSeries: Serie[];
 
-  arrSeries: Serie[] = new Array();
-  constructor() { }
+  constructor() {
+    this.arrSeries = new Array(
+      {
+        id: 1,
+        title: 'La vida es bella',
+        creador: 'autor1'
+
+      },
+      {
+        id: 2,
+        title: 'El grinch',
+        creador: 'autor2'
+
+      },
+      {
+        id: 3,
+        title: 'El hombre manos de tijera',
+        creador: 'autor3'
+
+      }
+    )
+  }
 
   ngOnInit(): void {
 
