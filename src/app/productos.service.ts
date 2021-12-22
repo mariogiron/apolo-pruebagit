569b8ecd4439ede6c7975fe8a8c4b296991710ed
Mario Girón <mariogironm@gmail.com>
2021-12-22 15:54:35 +0100

    Métodos getAll y create

--- src/app/productos.service.ts
+++ src/app/productos.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
-  constructor() { }
+  private baseUrl: string;
+
+  constructor(
+    private http: HttpClient
+  ) {
+    this.baseUrl = 'https://peticiones.online/api/products';
+  }
+
+  getAll(): Promise<any> {
+    return firstValueFrom(
+      this.http.get<any>(this.baseUrl)
+    )
+  }
+
+  create(formValue: any): Promise<any> {
+    return firstValueFrom(
+      this.http.post<any>(this.baseUrl, formValue)
+    );
+  }
+
 }
