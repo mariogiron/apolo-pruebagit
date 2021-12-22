fb682e3f64e480a87457adb673b14f742a99a416
Soraya Mobe <soraya.mobe@gmail.com>
2021-12-22 15:11:11 +0000

    creado metodo getAll

--- src/app/series.service.ts
+++ src/app/series.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Serie } from './interfaces/serie.interface';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeriesService {
 
-
-  constructor() {
-
+  private baseUrl: string;
+  constructor(
+    private httpClient: HttpClient
+  ) {
+    this.baseUrl = 'peticiones.online/api/series';
 
   }
+
+  getAll(): Promise<Serie[]> {
+    return lastValueFrom(this.httpClient.get<Serie[]>(this.baseUrl));
+  }
+
+
 }
